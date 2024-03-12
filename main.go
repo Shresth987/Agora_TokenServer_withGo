98e0b6730d223b19d037a4d7c18ad4314195377d
Shresth Sachan <93054395+Shresth987@users.noreply.github.com>
2024-03-12 16:47:05 +0530

    Update main.go

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ var appID string
 var appCertificate string
 
 func init() {
-	os.Setenv("APP_ID", "************ Put Your APP_ID Here ************")
-	os.Setenv("APP_CERTIFICATE", "************ Put Your APP_ID Here ************")
+	os.Setenv("APP_ID", "804f069ab8ef4876836865d07d8afd40")
+	os.Setenv("APP_CERTIFICATE", "ad2ae1d13a144294b1ee53bb1d1c2185")
 	// loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
 		log.Print("No .env file found")
